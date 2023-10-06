c5dd7c3c30f9309de700bee49bee8b49e8e23761
DaSherman0210 <tgerman181@gmail.com>
2023-10-06 16:21:43 -0500

    feat: :construction: Building frontend
    Building frontend page

---
+++ frontend/src/components/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/dashboard.css";
+
+const Dashboard = () =>{
+    return (
+        <div className="main">
+            <h1>SOY LA PÁGINA PRINCIPAL</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "../assets/logoKario.png";
+import { useNavigate } from "react-router-dom";
+import { Button } from "semantic-ui-react";
+import "../assets/css/login.css";
+
+const Login = () => {
+    const navigate = useNavigate();
+    return(
+        <div className="login">
+            <div className="miniLogin">
+            <div className="card">
+            <img src={logo} alt="hola" />
+            <p className="media">MEDIA</p>
+            <h2>Bienvenido al panel digital de KARIO Media</h2>
+            <p className="text">Por favor ingresa los siguientes datos para ingresar a la plataforma</p>
+            <form>
+                <div className="user">
+                    <label >Usuario</label>
+                    <input type="text" />
+                </div>
+                <div className="password">
+                    <label>Contraseña</label>
+                    <input type="text" />
+                </div>
+                <Button className="button" onClick={()=>{
+                    navigate("/dashboard")
+                }}>Ingresar al panel</Button>  
+
+            </form>
+            <p className="ultimoText">Tienes problemas para ingresar? Por favor contactarse con asistencia técnica lo más pronto posible</p>
+        </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
