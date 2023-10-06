bec73fd0d1a5e86287f9e0876a2a44556b3e3785
DaSherman0210 <tgerman181@gmail.com>
2023-10-06 16:24:23 -0500

    feat: :sparkles: Validate Documents
    Added the file validate documents

--- backend/src/middlewares/validate.documents.js
+++ backend/src/middlewares/validate.documents.js
@@ -1,1 +1,17 @@
 import { validationResult } from "express-validator";
+
+const validateDocuments = (req, res, next) => {
+   try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json(errors)
+      }
+      next();
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+export {
+    validateDocuments
+}
