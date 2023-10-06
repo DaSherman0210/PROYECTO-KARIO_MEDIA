368db4b9855ba11d74bd5e2b1376c22d05d9b404
DaSherman0210 <tgerman181@gmail.com>
2023-10-06 16:22:29 -0500

    feat: :bricks: Modificacion of routes
    Structure modificated that improve the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Login from './components/login';
+import Dashboard from './components/dashboard';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='body'>
+      <Router>
+        <Routes>
+          <Route exact path='/' Component={Login} />
+          <Route exact path='/dashboard' Component={Dashboard} />
+        </Routes>
+      </Router >
+    </div >
   );
 }
 
