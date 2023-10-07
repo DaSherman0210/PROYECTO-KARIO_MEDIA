8793b0492f4abb83146a060e6459b0999fad0ada
DaSherman0210 <tgerman181@gmail.com>
2023-10-07 15:25:23 -0500

    feat: :sparkles: Added a component
    Added a component to the App file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
+import React from 'react';
+import Logo from './components/logo';
 import Login from './components/login';
 import Dashboard from './components/dashboard';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from './components/main';
 
 function App() {
   return (
     <div className='body'>
       <Router>
         <Routes>
-          <Route exact path='/' Component={Login} />
+          <Route exact path='/login' Component={Login} />
           <Route exact path='/dashboard' Component={Dashboard} />
+          <Route exact path='/' Component={Logo} />
+          <Route exact path='/main' Component={Main} />
         </Routes>
-      </Router >
+      </Router>
     </div >
   );
 }
