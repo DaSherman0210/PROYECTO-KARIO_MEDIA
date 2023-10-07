efe1e5b04137c24df228d6f3873d7a343d5c6e04
DaSherman0210 <tgerman181@gmail.com>
2023-10-07 15:26:15 -0500

    feat: :sparkles: Generate JWT file
    Added the JWT file generator

--- backend/src/helpers/generate.JWT.js
+++ backend/src/helpers/generate.JWT.js
@@ -1,1 +1,21 @@
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const generateJWT = (uid = '') => {
+    return new Promise((resolve, reject) => {
+        const payload = { uid };
+        jwt.sign(payload, process.env.PRIVATE_OR_SECRET_KEY, {
+            expiresIn: '4h'
+        }, (err, token) => {
+            if (err) {
+                console.log(err, "Error al generar token. GENERATE.JWT");
+                reject('Problemas al generar JSON WEB TOKEN.');
+            } else {
+                resolve(token);
+            }
+        })
+    })
+}
+
+export default generateJWT;
