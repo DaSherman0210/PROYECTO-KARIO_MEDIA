94f1369b200e820c9e9e8b8864041d0154a82b2b
josuegiraldos <jggiraldosuarez2411@gmail.com>
2023-10-08 13:56:43 -0500

    perf: :zap: Code review
    Improve code CRUD usuarios

--- backend/src/controllers/usuarios.controllers.js
+++ backend/src/controllers/usuarios.controllers.js
@@ -9,142 +9,183 @@ const nombrebase = "Kario";
 const client = new MongoClient(bases);
 
 const connectToMongo = async () => {
-    try {
-        await client.connect();
-        const db = client.db(nombrebase);
-        const collection = db.collection('usuarios');
-        return collection;
-    } catch (error) {
-        console.log(error, "Error al conectar a la base de datos. USUARIOS.CONTROLLERS");
-    }
-}
+  try {
+    await client.connect();
+    const db = client.db(nombrebase);
+    const collection = db.collection("usuarios");
+    return collection;
+  } catch (error) {
+    throw new Error(
+      error,
+      "Error al conectar a la base de datos. USUARIOS.CONTROLLERS"
+    );
+  }
+};
 
 const getUsuarios = async (req, res) => {
-    try {
-        const collection = await connectToMongo();
-        const query = { estado: true };
-        const result = await collection.find(query).toArray();
-        res.status(202).json({
-            msg: "Usuarios obtenidos de la base de datos.",
-            result
-        })
-    } catch (error) {
-        console.log(error, "Error al obtener los usuarios de la base de datos. USUARIOS.CONTROLLERS");
-        res.status(404).json({
-            msg: "Error al obtener los usuarios de la base de datos.",
-        });
-    } finally {
-        client.close();
-    }
-}
+  try {
+    const collection = await connectToMongo();
+    const query = { estado: true };
+    const result = await collection.find(query).toArray();
+    res.status(202).json({
+      msg: "Usuarios obtenidos de la base de datos.",
+      result,
+    });
+  } catch (error) {
+    console.log(
+      error,
+      "Error al obtener los usuarios de la base de datos. USUARIOS.CONTROLLERS"
+    );
+    res.status(404).json({
+      msg: "Error al obtener los usuarios de la base de datos.",
+    });
+  } finally {
+    client.close();
+  }
+};
 
 const getUsuarioById = async (req, res) => {
-    try {
-        const collection = await connectToMongo();
-        const query = { estado: true };
-        const result = await collection.aggregate([
-            {
-                $match: {
-                    _id: new ObjectId(req.params.id),
-                    query
-                }
-            }
-        ]).toArray();
-        res.status(202).json({
-            msg: "Usuario obtenido por ID.",
-            result
-        })
-    } catch (error) {
-        console.log(error, "Error al obtener usuario por ID de la base de datos. USUARIOS.CONTROLLERS");
-        res.status(404).json({
-            msg: "Error al obtener usuario por ID de la base de datos",
-        });
-    } finally {
-        client.close();
+  try {
+    const collection = await connectToMongo();
+    const query = { _id: new ObjectId(req.params.id), estado: true };
+    const result = await collection.find(query).toArray();
+
+    if (result.length === 0) {
+      res.status(404).json({
+        msg: "El usuario solicitado por ID está inactivo o no se encuentra en la base de datos.",
+      });
+    } else {
+      res.status(202).json({
+        msg: "Usuario solicitado por ID.",
+        result,
+      });
     }
-}
+    client.close();
+  } catch (error) {
+    console.log(
+      error,
+      "Error al obtener usuario por ID de la base de datos. USUARIOS.CONTROLLERS"
+    );
+    res.status(404).json({
+      msg: "Error al obtener usuario por ID de la base de datos",
+    });
+  }
+};
 
 const postUsuario = async (req, res) => {
-    try {
-        const collection = await connectToMongo();
-        const { nombre, email, password, imagen } = req.body;
-        const usuario = {
-            nombre,
-            email,
-            password
-        }
-        if (imagen) {
-            usuario.imagen = imagen;
-        } else {
-            usuario.imagen = 'avatar.jpg';
-        }
-
-        const salt = bcryptjs.genSaltSync();
-        usuario.password = bcryptjs.hashSync(password, salt);
-
-        const result = await collection.insertOne(usuario);
-        res.status(202).json({
-            msg: "Usuario agregado correctamente a la base de datos.",
-            result
-        });
-    } catch (error) {
-        console.log(error, "Error al agregar un nuevo usuario a la base de datos. USUARIOS.CONTROLLERS");
-        res.status(404).json({
-            msg: "Error al agregar un nuevo usuario a la base de datos.",
-        });
-    } finally {
-        client.close();
+  try {
+    const collection = await connectToMongo();
+    const { nombre, email, password, imagen, rol, estado } = req.body;
+    const usuario = {
+      nombre: nombre,
+      email: email,
+      password: password,
+      imagen: imagen,
+      rol: rol,
+      estado: true,
+    };
+    if (imagen) {
+      usuario.imagen = imagen;
+    } else {
+      usuario.imagen = "avatar.jpg";
     }
-}
+
+    if (rol) {
+      usuario.rol = rol;
+    } else {
+      // ID del rol "USER"
+      usuario.rol = new ObjectId("652017a6ea25e24d9f8441f3");
+    }
+
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    const result = await collection.insertOne(usuario);
+    res.status(202).json({
+      msg: "Usuario agregado correctamente a la base de datos.",
+      usuario,
+      result,
+    });
+  } catch (error) {
+    console.log(
+      error,
+      "Error al agregar un nuevo usuario a la base de datos. USUARIOS.CONTROLLERS"
+    );
+    res.status(404).json({
+      msg: "Error al agregar un nuevo usuario a la base de datos.",
+    });
+  } finally {
+    client.close();
+  }
+};
 
 const deleteUsuario = async (req, res) => {
-    try {
-        const collection = await connectToMongo();
-        const id = { _id: new ObjectId(req.params.id) };
-        const update = { $set: { estado: false } };
-        const result = await collection.updateOne(id, update);
-        res.status(202).json({
-            msg: "Usuario eliminado correctamente."
-        })
-    } catch (error) {
-        console.log(error, "Error al eliminar el usuario de la base de datos. USUARIOS.CONTROLLERS");
-        res.status(404).json({
-            msg: "Error al eliminar el usuario de la base de datos",
-        });
-    } finally {
-        client.close();
-    }
-}
+  try {
+    const collection = await connectToMongo();
+    const id = { _id: new ObjectId(req.params.id) };
+    const update = { $set: { estado: false } };
+    const result = await collection.updateOne(id, update);
+    res.status(202).json({
+      msg: "Usuario eliminado correctamente.",
+    });
+  } catch (error) {
+    console.log(
+      error,
+      "Error al eliminar el usuario de la base de datos. USUARIOS.CONTROLLERS"
+    );
+    res.status(404).json({
+      msg: "Error al eliminar el usuario de la base de datos",
+    });
+  } finally {
+    client.close();
+  }
+};
 
 const updateUsuario = async (req, res) => {
-    try {
-        const collection = await connectToMongo();
-        const { id } = req.params;
-        const { _id, password, ...resto } = req.body;
+  try {
+    const collection = await connectToMongo();
+    const { id } = req.params;
+    const { password, ...resto } = req.body;
 
-        if (password) {
-            const salt = bcryptjs.genSaltSync();
-            resto.password = bcryptjs.hashSync(password, salt);
-        }
-        const result = await collection.findOne(id, resto, { new: true });
-        res.status(202).json({
-            msg: "Usuario actualizado correctamente.",
-            result
-        })
-    } catch (error) {
-        console.log(error, "Error al actualizar usuario en la base de datos. USUARIOS.CONTROLLERS");
-        res.status(404).json({
-            msg: "Error al actualizar usuario en la base de datos",
-        });
-    } finally {
-        client.close();
+    if (password) {
+      const salt = bcryptjs.genSaltSync();
+      resto.password = bcryptjs.hashSync(password, salt);
     }
-}
+
+    const filter = { _id: new ObjectId(id) };
+    const update = { $set: resto };
+
+    const result = await collection.updateOne(filter, update, {
+      returnOriginal: false,
+    });
+
+    if (result.modifiedCount === 1) {
+      res.status(202).json({
+        msg: "Usuario actualizado correctamente.",
+        update,
+      });
+    } else {
+      res.status(404).json({
+        msg: "Usuario no encontrado o no se pudo actualizar.",
+      });
+    }
+  } catch (error) {
+    console.error(
+      error,
+      "Error al actualizar usuario en la base de datos. USUARIOS.CONTROLLERS"
+    );
+    res.status(500).json({
+      msg: "Error al actualizar usuario en la base de datos",
+    });
+  } finally {
+    client.close();
+  }
+};
 
 export {
-    getUsuarios,
-    getUsuarioById,
-    postUsuario,
-    deleteUsuario,
-    updateUsuario
-}
+  getUsuarios,
+  getUsuarioById,
+  postUsuario,
+  deleteUsuario,
+  updateUsuario,
+};
--- backend/src/routes/usuarios.routes.js
+++ backend/src/routes/usuarios.routes.js
@@ -2,31 +2,54 @@ import { Router } from "express";
 import { check } from "express-validator";
 import { validateDocuments } from "../middlewares/validate.documents.js";
 import { validateJWT } from "../middlewares/validate.jwt.js";
-import { isAdminRole } from "../middlewares/validate.role.js"
-import { getUsuarios, getUsuarioById, postUsuario, deleteUsuario, updateUsuario } from "../controllers/usuarios.controllers.js";
+import { isAdminRole } from "../middlewares/validate.role.js";
+import {
+  getUsuarios,
+  getUsuarioById,
+  postUsuario,
+  deleteUsuario,
+  updateUsuario,
+} from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
 router.get("/", getUsuarios);
 router.get("/:id", getUsuarioById);
-router.post("/", [
-    validateJWT,
-    isAdminRole,
-    check('nombre', 'El nombre del usuario es obligatorio. USUARIOS.ROUTES').not().isEmpty(),
-    check('email', 'Email inválido. USUARIOS.ROUTES').isEmail(),
-    check('password', "El password debe ser mínimo de 6 dígitos. USUARIOS.ROUTES").isLength({ min: 6 }),
-], postUsuario);
-router.delete('/:id', [
-    validateJWT,
-    isAdminRole,
-    check('id', 'No es un ID válido. USUARIOS.ROUTES').isMongoId(),
-    validateDocuments
-], deleteUsuario);
-router.put('/:id', [
-    validateJWT,
-    isAdminRole,
-    check('id', 'No es un ID válido. USUARIOS.ROUTES').isMongoId(),
-    validateDocuments
-], updateUsuario);
+router.post(
+  "/",
+  [
+    // validateJWT,
+    // isAdminRole,
+    check("nombre", "El nombre del usuario es obligatorio. USUARIOS.ROUTES")
+      .not()
+      .isEmpty(),
+    check("email", "Email inválido. USUARIOS.ROUTES").isEmail(),
+    check(
+      "password",
+      "El password debe ser mínimo de 6 dígitos. USUARIOS.ROUTES"
+    ).isLength({ min: 6 }),
+  ],
+  postUsuario
+);
+router.delete(
+  "/:id",
+  [
+    // validateJWT,
+    // isAdminRole,
+    check("id", "No es un ID válido. USUARIOS.ROUTES").isMongoId(),
+    validateDocuments,
+  ],
+  deleteUsuario
+);
+router.put(
+  "/:id",
+  [
+    // validateJWT,
+    // isAdminRole,
+    check("id", "No es un ID válido. USUARIOS.ROUTES").isMongoId(),
+    validateDocuments,
+  ],
+  updateUsuario
+);
 
 export default router;
