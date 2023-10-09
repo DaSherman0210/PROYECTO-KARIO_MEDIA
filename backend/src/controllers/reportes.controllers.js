fd24719ca391d314196056a3a846b4d7e2596fbc
DaSherman0210 <tgerman181@gmail.com>
2023-10-09 09:32:16 -0500

    feat: :sparkles: Reportes CRUD
    Done the reportes CRUD
    
    }

---
+++ backend/src/controllers/reportes.controllers.js
@@ -0,0 +1,171 @@
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const bases = process.env.MONGO_URI;
+const nombrebase = "Kario";
+const client = new MongoClient(bases);
+
+const connectToMongo = async () => {
+  try {
+    await client.connect();
+    const db = client.db(nombrebase);
+    const collection = db.collection("reportes");
+    return collection;
+  } catch (error) {
+    throw new Error(
+      error,
+      "Error al conectar a la base de datos. REPORTES.CONTROLLERS"
+    );
+  }
+};
+
+const getReportes = async (req, res) => {
+    try {
+        const collection = await connectToMongo();
+        const query = { estado: true };
+        const result = await collection.find(query).toArray();
+        res.status(202).json({
+            msg: "Reportes obtenidos de la base de datos. REPORTES.CONTROLLERS",
+            result
+        });
+    } catch (error) {
+        console.log(
+          error,
+          "Error al obtener los reportes de la base de datos. REPORTES.CONTROLLERS"
+        );
+        res.status(404).json({
+          msg: "Error al obtener los reportes de la base de datos.",
+        });
+      } finally {
+        client.close();
+      }
+}
+
+const getReporteById = async (req, res) => {
+    try {
+        const collection = await connectToMongo();
+        const query = { _id: new ObjectId(req.params.id), estado: true };
+        const result = await collection.findOne(query).toArray();
+
+        if (result.length === 0) {
+            res.status(404).json({
+              msg: "El reporte solicitado por ID está inactivo o no se encuentra en la base de datos.",
+            });
+          } else {
+            res.status(202).json({
+              msg: "Reporte solicitado por ID.",
+              result,
+            });
+          }
+          client.close();
+    } catch (error) {
+        console.log(
+          error,
+          "Error al obtener reporte por ID de la base de datos. REPORTE.CONTROLLERS"
+        );
+        res.status(404).json({
+          msg: "Error al obtener reporte por ID de la base de datos.",
+        });
+      }
+}
+
+const postReporte = async (req, res) => {
+    try {
+        const collection = await connectToMongo();
+        const { usuario, indicador, asunto, descripcion, estado } = req.body; 
+        const reporte = {
+            usuario, 
+            indicador, 
+            asunto, 
+            descripcion,
+            estado: true
+        };
+
+        const result = await collection.insertOne(reporte);
+        res.status(202).json({
+            msg: "Reporte agregado correctamente a la base de datos.",
+            reporte,
+            result
+        });
+    } catch (error) {
+        console.log(
+          error,
+          "Error al agregar un nuevo reporte a la base de datos. INDICADORES.CONTROLLERS"
+        );
+        res.status(404).json({
+          msg: "Error al agregar un nuevo reporte a la base de datos.",
+        });
+      } finally {
+        client.close();
+      }
+}
+
+const deleteReporte = async (req, res) => {
+    try {
+        const collection = await connectToMongo();
+        const id = { _id: new ObjectId(req.params.id) };
+        const update = { $set: { estado: false }};
+        const result = await collection.updateOne(id, update);
+        res.status(202).json({
+            msg: "Reporte eliminado correctamente de la base de datos."
+        })
+    } catch (error) {
+        console.log(
+          error,
+          "Error al eliminar el reporte de la base de datos. REPORTES.CONTROLLERS"
+        );
+        res.status(404).json({
+          msg: "Error al eliminar el reporte de la base de datos",
+        });
+      } finally {
+        client.close();
+      }
+}
+
+const updateReporte = async (req, res) => {
+    try {
+        const collection = await connectToMongo();
+        const { usuario, indicador, asunto, descripcion, estado } = req.body; 
+        const reporteUpdated = {
+            usuario, 
+            indicador, 
+            asunto, 
+            descripcion,
+            estado
+        };
+        const filter = { _id: new ObjectId(req.params.id) };
+        const result = await collection.updateOne(filter, { $set: reporteUpdated });
+
+        if (result.matchedCount === 0) {
+            res.status(404).json({
+              msg: "No se encontró el reporte para actualizar.",
+            });
+          } else {
+            res.status(202).json({
+              msg: "Reporte actualizado correctamente.",
+              reporteUpdated,
+              result,
+            });
+          }
+    } catch (error) {
+        console.error(
+          error,
+          "Error al actualizar reporte en la base de datos. REPORTES.CONTROLLERS"
+        );
+        res.status(500).json({
+          msg: "Error al actualizar reporte en la base de datos",
+        });
+      } finally {
+        client.close();
+      }
+}
+
+export {
+    getReportes,
+    getReporteById,
+    postReporte,
+    deleteReporte,
+    updateReporte
+}
---
+++ backend/src/routes/reportes.routes.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { validateDocuments } from "../middlewares/validate.documents.js";
+import { validateJWT } from "../middlewares/validate.jwt.js";
+import { isAdminRole } from "../middlewares/validate.role.js";
+import { getReportes, getReporteById, postReporte, deleteReporte, updateReporte } from "../controllers/reportes.controllers.js"
+
+const router = Router();
+
+router.get('/', getReportes);
+router.get('/:id', [
+    check('id', 'No es un ID válido. REPORTES.ROUTES').isMongoId(),
+    validateDocuments
+], getReporteById);
+router.post('/', [
+    validateJWT,
+    isAdminRole,
+    check('usuario', 'El usuario es obligatorio. REPORTES.ROUTES').not().isEmpty(),
+    check('usuario', 'ID de usuario no válido. REPORTES.ROUTES').isMongoId(),
+    check('indicador', 'El indicador es obligatorio. REPORTES.ROUTES').not().isEmpty(),
+    check('indicador', 'ID de indicador no válido. REPORTES.ROUTES').isMongoId(),
+    check('asunto', 'El asunto es obligatorio. REPORTES.ROUTES').not().isEmpty(),
+    check('descripcion', 'La descripcion es obligatoria. REPORTES.ROUTES').not().isEmpty(),
+    validateDocuments
+], postReporte);
+router.delete('/:id', [
+    validateJWT,
+    isAdminRole,
+    check('id', 'No es un ID válido. REPORTES.ROUTES').isMongoId(),
+    validateDocuments
+], deleteReporte);
+router.put('/:id', [
+    validateJWT,
+    isAdminRole,
+    check('id', 'No es un ID válido. REPORTES.ROUTES').isMongoId(),
+    validateDocuments
+], updateReporte);
+
+export default router;
+
+/* 
+const reporteUpdated = {
+            usuario, 
+            indicador, 
+            asunto, 
+            descripcion,
+            estado
+        };
+*/
